18bf538aea98f6dae8ce42b52c9e8a02e1e19bfc
scarknighty12 <ethanchan02788@gmail.com>
2025-01-23 07:51:08 +0800

    accom
    made dropdown for accom

--- INCLUDES/adminNav.php
+++ INCLUDES/adminNav.php
@@ -1,10 +1,42 @@
 <div class="topNav">
   <!-- navigation bar -->
   <nav class="navbar">
-    <a href="../../MODULES/ADMIN_MODULE/adminHome.php">HOME</a>
-    <a href="">ACCOMMODATION</a>
-    <a href="">ACTIVITY</a>
-    <a href="">FOOD</a>
+    <a href="../../../MODULES/ADMIN_MODULE/adminHome.php">HOME</a>
+
+
+    <div class="dropdown">
+      <a href="javascript:void(0)">ACCOMMODATION
+      <i class="fa fa-caret-down"></i>
+      </a>
+      <div class="dropdown-content">
+        <a href="../../MODULES/ADMIN_MODULE/ACCOMMODATION/accommodationForm.php">Add New Accommodation</a>
+        <a href="../../MODULES/ADMIN_MODULE/ACCOMMODATION/accommodationList.php">Manage Accommodation</a>
+      </div>
+    </div>
+
+
+    <div class="dropdown">
+      <a href="javascript:void(0)">ACTIVITY
+      <i class="fa fa-caret-down"></i>
+      </a>
+      <div class="dropdown-content">
+        <a href="../../../MODULES/ADMIN_MODULE/ACTIVITY/page1.php">Add New Activity</a>
+        <a href="../../../MODULES/ADMIN_MODULE/ACTIVITY/page1.php">Manage Activity</a>
+      </div>
+    </div>
+
+
+    <div class="dropdown">
+      <a href="javascript:void(0)">FOOD
+      <i class="fa fa-caret-down"></i>
+      </a>
+      <div class="dropdown-content">
+        <a href="../../../MODULES/ADMIN_MODULE/FOOD/page1.php">Add New Food</a>
+        <a href="../../../MODULES/ADMIN_MODULE/FOOD/page1.php">Manage Food</a>
+      </div>
+    </div>
+
+
     <a href="">REVIEW</a>
     <a href="../../MODULES/USER_MANAGEMENT_MODULE/profile.php">PROFILE</a>
   </nav>
