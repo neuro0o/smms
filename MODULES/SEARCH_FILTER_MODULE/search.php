0989be7a5ca4222b5a10b8db20a16044655eb094
neuro0o <146324467+neuro0o@users.noreply.github.com>
2025-01-23 20:02:55 +0800

    activity filter
    - search filter for activity done

--- MODULES/SEARCH_FILTER_MODULE/search.php
+++ MODULES/SEARCH_FILTER_MODULE/search.php
@@ -1,92 +1,119 @@
 <?php
-  session_start();
-  include_once("../../CONFIG/config.php");
+session_start();
+include_once("../../CONFIG/config.php");
 
-  // validate input search text and split into keywords using explode function
-  $search_text = '';
-  if (!empty($_POST["search_text"])) {
+// Validate input search text and split into keywords using explode function
+$search_text = '';
+if (!empty($_POST["search_text"])) {
     $search_text = trim($_POST["search_text"]);
-  }
+}
 
-  $keywords = explode(" ", $search_text);
+$keywords = explode(" ", $search_text);
 
-  // initialize SQL query search conditions using an array
-  $search_conditions = [];
-  foreach ($keywords as $keyword) {
-    if(!empty($keyword)) {
-      $search_conditions[] = "foodName LIKE '%" .mysqli_real_escape_string($conn, $keyword) . "%'";
+// Initialize SQL query search conditions using an array
+$search_conditions_food = [];
+$search_conditions_accommodation = [];
+$search_conditions_activity = [];
+
+foreach ($keywords as $keyword) {
+    if (!empty($keyword)) {
+        $escaped_keyword = mysqli_real_escape_string($conn, $keyword);
+        $search_conditions_food[] = "foodName LIKE '%" . $escaped_keyword . "%'";
+        $search_conditions_accommodation[] = "accommodationName LIKE '%" . $escaped_keyword . "%'";
+        $search_conditions_activity[] = "activityName LIKE '%" . $escaped_keyword . "%'";
     }
-  }
+}
 
-  // execute SQL query based on search conditions and display the results
-  $sql = "SELECT * FROM food WHERE " . implode(" OR", $search_conditions);
-  $result = mysqli_query($conn, $sql);
+// Execute SQL queries based on search conditions and display the results
+$sql_food = "SELECT * FROM food WHERE " . implode(" OR ", $search_conditions_food);
+$sql_accommodation = "SELECT * FROM accommodation WHERE " . implode(" OR ", $search_conditions_accommodation);
+$sql_activity = "SELECT * FROM activity WHERE " . implode(" OR ", $search_conditions_activity);
 
+$result_food = mysqli_query($conn, $sql_food);
+$result_accommodation = mysqli_query($conn, $sql_accommodation);
+$result_activity = mysqli_query($conn, $sql_activity);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- cdn icon link -->
+    <!-- CDN icon link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
-    <!-- utils css file -->
+    <!-- Utils CSS file -->
     <link rel="stylesheet" href="../../../SMMS/CSS/MISC/utils.css">
 
-    <!-- headerBanner css file -->
+    <!-- HeaderBanner CSS file -->
     <link rel="stylesheet" href="../../../SMMS/CSS/MISC/headerBanner.css">
 
-    <!-- topNav css file -->
+    <!-- TopNav CSS file -->
     <link rel="stylesheet" href="../../../SMMS/CSS/MISC/topNav.css">
 
-    <!-- topHeader css file -->
+    <!-- TopHeader CSS file -->
     <link rel="stylesheet" href="../../../SMMS/CSS/MISC/topHeader.css">
 
-    <!-- userHome css file -->
+    <!-- UserHome CSS file -->
     <link rel="stylesheet" href="../../../SMMS/CSS/USER/userHome.css">
 
     <title>SEARCH</title>
-  </head>
+</head>
 
-  <body>
+<body>
 
-    <!-- include topNav.php -->
-    <?php include '../../INCLUDES/topHeader.php'; ?>
-    
-    <!-- include userNav.php -->
-    <?php include '../../INCLUDES/userNav.php'; ?>
+<!-- Include topNav.php -->
+<?php include '../../INCLUDES/topHeader.php'; ?>
 
-    <!-- include headerBanner.php -->
-    <?php include '../../INCLUDES/headerBanner.php';?>
-    
-    <section class="user-home">
-      <div id="searchMessage">
+<!-- Include userNav.php -->
+<?php include '../../INCLUDES/userNav.php'; ?>
+
+<!-- Include headerBanner.php -->
+<?php include '../../INCLUDES/headerBanner.php'; ?>
+
+<section class="user-home">
+    <div id="searchMessage">
         <p>SEARCH RESULT:</p>
-      </div>
+    </div>
 
-      <div class="searchResult-container">
+    <div class="searchResult-container">
         <?php
-          if($result) {
-            if(mysqli_num_rows($result) > 0) {
-              while($row = mysqli_fetch_assoc(($result))) {
-                echo "&nbsp;&nbsp;&nbsp;&nbsp;" . "<a class='searchResult' href='" . BASE_URL . "/MODULES/FOOD_MODULE/foodList.php?id=" . htmlspecialchars($row['foodID']) . "'>" . htmlspecialchars($row['foodName']) . "</a><br>";
-              }
+        // Display food results
+        if ($result_food) {
+            if (mysqli_num_rows($result_food) > 0) {
+                while ($row = mysqli_fetch_assoc($result_food)) {
+                    echo "&nbsp;&nbsp;&nbsp;&nbsp;" . "<a class='searchResult' href='" . BASE_URL . "/MODULES/FOOD_MODULE/foodList.php?id=" . htmlspecialchars($row['foodID']) . "'>" . htmlspecialchars($row['foodName']) . "</a><br>";
+                }
             }
-            else {
-              echo "<p>Sorry, no result for '" . htmlspecialchars($search_text) . "'</p>";
+        }
+
+        // Display accommodation results
+        if ($result_accommodation) {
+            if (mysqli_num_rows($result_accommodation) > 0) {
+                while ($row = mysqli_fetch_assoc($result_accommodation)) {
+                    echo "&nbsp;&nbsp;&nbsp;&nbsp;" . "<a class='searchResult' href='" . BASE_URL . "/MODULES/ACCOMMODATION_MODULE/accommodationList.php?id=" . htmlspecialchars($row['accommodationID']) . "'>" . htmlspecialchars($row['accommodationName']) . "</a><br>";
+                }
             }
-          }
-          else {
-            echo "<p>Error executing query: " . mysqli_error($conn) . "</p>";
-          }
+        }
+
+        // Display activity results
+        if ($result_activity) {
+            if (mysqli_num_rows($result_activity) > 0) {
+                while ($row = mysqli_fetch_assoc($result_activity)) {
+                    echo "&nbsp;&nbsp;&nbsp;&nbsp;" . "<a class='searchResult' href='" . BASE_URL . "/MODULES/ACTIVITY_MODULE/activityList.php?id=" . htmlspecialchars($row['activityID']) . "'>" . htmlspecialchars($row['activityName']) . "</a><br>";
+                }
+            }
+        }
+
+        // If no results found in any category
+        if ( !mysqli_num_rows($result_food) && !mysqli_num_rows($result_accommodation) && !mysqli_num_rows($result_activity)) {
+            echo "<p>Sorry, no results for '" . htmlspecialchars($search_text) . "'</p>";
+        }
         ?>
         <br><a class='homePage-link' href='<?php echo BASE_URL . "/MODULES/USER_MANAGEMENT_MODULE/userHome.php"; ?>'>Back to HOME</a>
-      </div>
-    </section>
+    </div>
+</section>
 
-
-  </body>
+</body>
 </html>
